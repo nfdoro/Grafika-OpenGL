db76bbcf21ffffb5d821656a7d73de0fcd4e18e2
nfdoro <nfdoro@gmail.com>
2025-03-04 12:45:59 +0200

    Akkor se lesz szine ha 0 ra irom a vertezAttribArray-t

--- GrafikaSzemi/GrafikaSzemi/Program.cs
+++ GrafikaSzemi/GrafikaSzemi/Program.cs
@@ -143,9 +143,9 @@ namespace Szeminarium1
          
             uint colors = Gl.GenBuffer();
             Gl.BindBuffer(GLEnum.ArrayBuffer, colors);
-           // Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
+            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
             Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
-            Gl.EnableVertexAttribArray(1);
+            Gl.EnableVertexAttribArray(0);
 
 
            
