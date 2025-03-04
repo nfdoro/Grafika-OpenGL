eab926656ff6715e878f5179f965ab34d1c973fb
nfdoro <nfdoro@gmail.com>
2025-03-04 12:51:40 +0200

    ha kommentelem a szineknel az Enable-t, akkor szinten nincs   szint

--- GrafikaSzemi/GrafikaSzemi/Program.cs
+++ GrafikaSzemi/GrafikaSzemi/Program.cs
@@ -139,13 +139,13 @@ namespace Szeminarium1
             Gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
             Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexArray.AsSpan(), GLEnum.StaticDraw);
             Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, null);
-            Gl.EnableVertexAttribArray(1);
+            Gl.EnableVertexAttribArray(0);
          
             uint colors = Gl.GenBuffer();
             Gl.BindBuffer(GLEnum.ArrayBuffer, colors);
             Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
             Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
-            Gl.EnableVertexAttribArray(1);
+            //Gl.EnableVertexAttribArray(1);
 
 
            
